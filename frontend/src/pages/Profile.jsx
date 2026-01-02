484fe99512ae76adb2c2eb1b80616a4e0ff7f389
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2026-01-02 13:58:38 +0530

    feat: update profile stats to display total likes and comments instead of followers and following

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -60,8 +60,8 @@ const Profile = () => {
                             {/* Stats */}
                             <div className="flex gap-10 text-sm">
                                 <span><strong>{posts.length}</strong> posts</span>
-                                <span><strong>161</strong> followers</span>
-                                <span><strong>1,859</strong> following</span>
+                                <span><strong>{posts.likes.length}</strong> Total Likes</span>
+                                <span><strong>{posts.comments.length}</strong> Total Comments</span>
                             </div>
 
                             {/* Bio */}
